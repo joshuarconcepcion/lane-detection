1ca9f9f7bf19f3dc2686c86276dd176fd15ef4f5
Joshua Concepcion <joshuarconcepcion@gmail.com>
2026-06-11 18:33:27 -0700

    Finished applyROI function

--- lane_detection.cpp
+++ lane_detection.cpp
@@ -13,4 +13,8 @@ cv::Mat applyROI(const cv::Mat% edges) {
     std::vector<std::vector<cv::Point>> polygons;
     polygons.push_back(poly);
     cv::fillPoly(mask, polygons, Scalar(255)); //paints poly white onto black mask
+
+    cv::Mat masked;
+    cv::bitwise_and(edges, mask, masked); //if pixel in mask is white, maintain same pixel in edges, but if pixel in mask is black, return black regardless of what corresponding pixel is in edges
+    return masked; //returns the region of interest
 };
